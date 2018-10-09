4599a4cbbf1df0ad30d6cc736320d2e5f6e1ddd8
WonPyoung Choi <cwp10@msn.com>
2018-10-09 19:19:27 +0900

    몬스터 이동

---
+++ Assets/Scripts/Monster.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour 
+{
+	private Rigidbody2D _rigidbody = null;
+
+	public float speed = 50.0f;
+
+	private void Awake()
+	{
+		_rigidbody = GetComponent<Rigidbody2D>();
+	}
+
+	private void Update()
+	{
+		_rigidbody.velocity = Vector2.down * speed * Time.deltaTime;
+	}
+}
