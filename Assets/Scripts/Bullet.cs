11c9c3c25b7f1f25c445391ccf4e8644e2c059cb
WonPyoung Choi <cwp10@msn.com>
2018-10-09 19:02:06 +0900

    총알 제작

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour 
+{
+	private Rigidbody2D _rigidbody = null;
+
+	public float speed = 500.0f;
+
+	private void Awake () 
+	{
+		_rigidbody = GetComponent<Rigidbody2D>();
+	}
+
+	private void Update()
+	{
+		_rigidbody.velocity = new Vector2(0, speed * Time.deltaTime);
+	}
+}
