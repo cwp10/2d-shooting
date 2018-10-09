c0997b6212a8e50b8210e6b1c6865ab8977e6b1c
WonPyoung Choi <cwp10@msn.com>
2018-10-09 19:39:13 +0900

    몬스터 히트 상태 처리

--- Assets/Scripts/Monster.cs
+++ Assets/Scripts/Monster.cs
@@ -4,25 +4,54 @@ using UnityEngine;
 
 public class Monster : MonoBehaviour 
 {
+	public enum State
+	{
+		Idle,
+		Hit,
+	}
+
 	private Rigidbody2D _rigidbody = null;
+	private Animator _animator = null;
+	private State _state = State.Idle;
 
 	public float speed = 50.0f;
 
 	private void Awake()
 	{
 		_rigidbody = GetComponent<Rigidbody2D>();
+		_animator = GetComponent<Animator>();
+	}
+
+	private void OnEnable()
+	{
+		_state = State.Idle;
 	}
 
 	private void Update()
 	{
-		_rigidbody.velocity = Vector2.down * speed * Time.deltaTime;
+		if(_state == State.Idle)
+		{
+			_rigidbody.velocity = Vector2.down * speed * Time.deltaTime;
+		}
+		else
+		{
+			_rigidbody.velocity = Vector2.zero;
+		}
 	}
 
 	private void OnTriggerEnter2D(Collider2D other)
 	{
-		if(other.tag == "Bullet")
+		if(other.tag == "Bullet" && _state == State.Idle)
 		{
-			Destroy(this.gameObject);
+			_state = State.Hit;
+			_animator.SetTrigger("Hit");
+			
+			Invoke("Dead", 1.0f);
 		}
+	}
+
+	private void Dead()
+	{
+		Destroy(this.gameObject);
 	}
 }
