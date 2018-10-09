828e1d540025b13893001e4baee3be0bca7b4e2f
WonPyoung Choi <cwp10@msn.com>
2018-10-09 16:39:14 +0900

    총알이 화면 밖으로 나가면 삭제

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -5,16 +5,25 @@ using UnityEngine;
 public class Bullet : MonoBehaviour 
 {
 	private Rigidbody2D _rigidbody = null;
+	private Camera _camera = null;
 
 	public float speed = 500.0f;
 
 	private void Awake () 
 	{
+		_camera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
 		_rigidbody = GetComponent<Rigidbody2D>();
 	}
 
 	private void Update()
 	{
 		_rigidbody.velocity = new Vector2(0, speed * Time.deltaTime);
+
+		Vector2 pos = _camera.WorldToScreenPoint(_rigidbody.position);
+
+		if(pos.y > Screen.height)
+		{
+			Destroy(this.gameObject);
+		}
 	}
 }
